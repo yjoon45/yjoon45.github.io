6ecb1ff1f12a929e75934f4ca961886a650c4e4d
yogesh <yjoon45@gmail.com>
2022-07-17 21:49:36 +0530

    changes

--- blfc/js/main.js
+++ blfc/js/main.js
@@ -6,9 +6,38 @@ function toggleNav() {
   $('.bflc-nav').classList.toggle('opennav');
 }
 
-$('.blfc-mobileBtn').addEventListener('click', toggleNav);
-$('.blfc-closeMobileBtn').addEventListener('click', toggleNav);
+if ($('.blfc-mobileBtn')) {
+  $('.blfc-mobileBtn').addEventListener('click', toggleNav);
+}
+
+if ($('.blfc-closeMobileBtn')) {
+  $('.blfc-closeMobileBtn').addEventListener('click', toggleNav);
+}
+
+if ($('.bflc-nav')) {
+  $('.bflc-nav').addEventListener('click', toggleNav);
+}
 
 AOS.init({
   once: true,
+});
+
+document.querySelectorAll('.bflc-nav').forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    var el = document.querySelector(e.target.getAttribute('href'));
+    var headerHeight = window.matchMedia('(min-width: 992px)').matches
+      ? 107
+      : 85;
+
+    if (el) {
+      var offset = el.offsetTop - headerHeight;
+
+      window.scrollTo({
+        top: offset,
+        behavior: 'smooth',
+      });
+    }
+  });
 });
