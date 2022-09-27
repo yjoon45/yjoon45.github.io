c2b0f9eeabc7f8672fc11b189bc5a695903d1ac9
Yogesh <yjoon45@gmail.com>
2022-09-27 22:32:43 +0530

    new WhereAt

---
+++ whereat/js/main.bundle.js
@@ -0,0 +1,89 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+$(function () {
+  $('.tokenSlide').slick({
+    centerMode: true,
+    centerPadding: '200px',
+    slidesToShow: 3,
+    responsive: [{
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+        centerPadding: '20px',
+        slidesToScroll: 1,
+        centerMode: true,
+        infinite: true,
+        dots: false
+      }
+    }, {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: '20px',
+        centerMode: true,
+        dots: false
+      }
+    }]
+  });
+});
+$(function () {
+  $('.ourAdviser-slide').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: false
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
+    }]
+  });
+});
+$(function () {
+  $('.exploreAppSlide').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    fade: true,
+    responsive: [{
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        fade: true,
+        infinite: true,
+        dots: false
+      }
+    }]
+  });
+});
+$(function () {
+  $('#showNav').on('click', function () {
+    $('.wh-navbar').addClass('mobileNav');
+  });
+  $('.closeNav').on('click', function () {
+    $('.wh-navbar').removeClass('mobileNav');
+  });
+});
+/******/ })()
+;
